42de171e2a2a122d83ba5738867e433b0aca6d58
Fountain <fountainaigbona@gmail.com>
2024-12-02 13:00:05 +0100

    Add and Edit functionality of the todo list done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { db } from "./db_config.js";
 
 const app = express();
 const port = 3000;
@@ -7,12 +8,23 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
+db.connect();
 
-app.get("/", (req, res) => {
+// let items = [];
+
+async function getItems() {
+  // Gets all items from  the database
+  const result = await db.query("SELECT * FROM items");
+  const items = [];
+  
+  result.rows.forEach(item => {
+    items.push(item);
+  });
+  return items;
+};
+
+app.get("/", async (req, res) => {
+  const items = await getItems();
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
@@ -21,11 +33,26 @@ app.get("/", (req, res) => {
 
 app.post("/add", (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+  
+  try {
+    db.query("INSERT INTO items (title) VALUES ($1)", [item])
+  } catch (error) {
+    console.error("Error adding information into database: " + error.message);
+    res.status(500).send("Error occured while create todo");
+  }
+
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", (req, res) => {
+  try {
+    db.query("UPDATE items SET title = $1 WHERE id = $2", [req.body.updatedItemTitle, req.body.updatedItemId])
+    res.redirect("/");
+  } catch (error) {
+      console.error("Error updating todo: " + error.message);
+      res.status(500).send("Error occured while editing your todo");
+  }
+});
 
 app.post("/delete", (req, res) => {});
 
